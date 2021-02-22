bc801184e51b1626b5e8008cda190e9da03b331a
Дмитрий Хороших <marinad1814@yandex.ru>
2021-02-22 17:04:02 +0800

    A bit of renaming here and there, shouldn't cause major problems. To be honest, it shouldn't cause any problems at all!

---
+++ FoodRun/Food.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRunners
+{
+    class Food
+    {
+        public int X;
+        public int Y;
+        public char Character = 'F';
+        public ConsoleColor Color;
+        public int Value = 10;
+
+        public void FoodTeleport(Program.Map map)
+        {
+        }
+
+
+    }
+}
